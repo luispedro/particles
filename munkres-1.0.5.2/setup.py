968c56da2a09070dfde321f498682c14a64fc1a2
Andrej Savol <ajs171@pitt.edu>
2009-05-12 21:59:09 +0000

    added munkres package used for object tracking in *linktracks.py*

---
+++ munkres-1.0.5.2/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+# Distutils setup script for Munkres
+#
+# $Id: setup.py 7300 2008-03-17 01:06:08Z bmc $
+# ---------------------------------------------------------------------------
+
+from distutils.core import setup
+import re, munkres
+
+VERSION = str(munkres.__version__)
+(AUTHOR, EMAIL) = re.match('^(.*),\s*(.*)$', munkres.__author__).groups()
+URL = munkres.__url__
+LICENSE = munkres.__license__
+
+setup(
+    name="munkres",
+    version=VERSION,
+    description="munkres algorithm for the Assignment Problem",
+    url=URL,
+    license=LICENSE,
+    author=AUTHOR,
+    author_email=EMAIL,
+    py_modules=["munkres"],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Mathematics', 
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
