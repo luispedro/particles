d033faba7b0bda59c0e739bdd6edc8f62e6eeb45
Longzhu Shen <lshen@andrew.cmu.edu>
2009-05-13 22:32:22 +0000

    fixed the test on particles that show up only in one frame

--- tests/test_statisticalsummary.py
+++ tests/test_statisticalsummary.py
@@ -3,15 +3,32 @@ import particles.statisticalsummary
 import particles.position
 
 def test_statisticalsummary():
+    '''
+    General test for the compute_statistics function
+    '''
     tracks = particles.trackgeneration.generate_tracks('brownian',10,.4,.2,1,100)
     simulated,predicted = particles.statisticalsummary.compute_statistics(tracks,tracks)
     assert simulated == predicted
     assert simulated >= 0
 
-def test_singleframe():
-    tracks = (0,[particles.position.Position(0,0)])
-    simulated,predicted = particles.statisticalsummary.compute_statistics(tracks,tracks)
-    assert simulated == predicted == 0 
+def test_shortlived():
+    '''
+    This is to test the velocity function for particles that only show up in one
+    frame
+    '''
+    flag = 0
+    while 1 : 
+        tracks = particles.trackgeneration.generate_tracks('brownian',10,.4,.2,1,100)
+        zerotracks = []
+        for i in xrange(len(tracks)):
+            if len(tracks[i][1]) == 1:
+                 zerotracks.append(tracks[i])
+        if len(zerotracks) >= 2 : 
+            flag = 1
+        if flag :
+            vel  = particles.statisticalsummary.traj_vel(zerotracks)
+            break
+    assert vel == 0 
 
 
 
