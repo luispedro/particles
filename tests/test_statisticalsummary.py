db8a9030f87cf83d0910f8635a8812ebcb6fe3d0
Longzhu Shen <lshen@andrew.cmu.edu>
2009-05-13 21:19:51 +0000

    added test for particles that only show up once

--- tests/test_statisticalsummary.py
+++ tests/test_statisticalsummary.py
@@ -1,9 +1,17 @@
 import particles.trackgeneration
 import particles.statisticalsummary
+import particles.position
 
 def test_statisticalsummary():
     tracks = particles.trackgeneration.generate_tracks('brownian',10,.4,.2,1,100)
     simulated,predicted = particles.statisticalsummary.compute_statistics(tracks,tracks)
     assert simulated == predicted
     assert simulated >= 0
+
+def test_singleframe():
+    tracks = (0,[particles.position.Position(0,0)])
+    simulated,predicted = particles.statisticalsummary.compute_statistics(tracks,tracks)
+    assert simulated == predicted == 0 
+
+
 
